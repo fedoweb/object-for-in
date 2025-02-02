302ec63973efbab9777022373df7721e7487df2e
fedoweb <fedo.web@mail.ru>
2025-02-02 19:56:56 +0300

    add change homework

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,8 +1,8 @@
-import orderByProps from '../app';
+import orderByProps from "../app";
 
 test.each([
   [
-    {name: 'мечник', health: 10, level: 2, attack: 80, defence: 40},
+    {name: "мечник", health: 10, level: 2, attack: 80, defence: 40},
     ["name", "level"],
     [{key: "name", value: "мечник"}, {key: "level", value: 2}, {key: "attack", value: 80}, {key: "defence", value: 40}, {key: "health", value: 10}]
   ],
@@ -12,7 +12,7 @@ test.each([
     [{key: "attack", value: 80}, {key: "defence", value: 40}, {key: "health", value: 10}, {key: "level", value: 2}, {key: "name", value: "мечник"}]
   ],
   [
-    {name: 'мечник', health: 10, level: 2},
+    {name: "мечник", health: 10, level: 2},
     ["name", "level", "health"],
     [{key: "name", value: "мечник"}, {key: "level", value: 2}, {key: "health", value: 10}]
   ],
@@ -21,6 +21,6 @@ test.each([
     ["name", "level"],
     []
   ]
-])('testing function orderByProps',(obj, arr, result) => {
+])("testing function orderByProps",(obj, arr, result) => {
   expect(orderByProps(obj, arr)).toEqual(result);
 })
